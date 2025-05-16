20601c21997017295b865a4db7636e98c7cdbc20
Катерина <novoseleckakata@gmail.com>
2025-05-17 01:30:51 +0300

    Attempting to edit photo links

--- src/components/Services/OurServices.jsx
+++ src/components/Services/OurServices.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import ServiceCard from '../Services/ServiceCard';
-import styles from './OurServices.module.css'; 
+import styles from './OurServices.module.css';
 
 const OurServices = () => {
   const servicesData = [
     {
       id: 1,
       title: 'Система оцінки психологічного стану користувачів',
-      image: '/test.png',
+      image: '/PSYREHABB/test.png', // Оновлений шлях
       anchor: 'assessment',
     },
     {
       id: 2,
       title: 'Бібліотека інформаційних ресурсів',
-      image: '/educ.png',
+      image: '/PSYREHABB/educ.png', // Оновлений шлях
       anchor: 'library',
     },
     {
       id: 3,
       title: 'Платформа для обміну досвідом',
-      image: '/forum.png',
+      image: '/PSYREHABB/forum.png', // Оновлений шлях
       anchor: 'community',
     },
     {
       id: 4,
       title: 'Інструмент для ведення особистих записів',
-      image: '/emo.png',
+      image: '/PSYREHABB/emo.png', // Оновлений шлях
       anchor: 'emotion',
     },
   ];
 
- return (
+  return (
     <section id="services-section" className={styles.servicesSection}>
       <div className={styles.container}>
         <h2>Наші послуги</h2>
